8eaff9533dfe32d0c24299b3231923142f493a7f
ktmizugaki <ktmizugaki@gmail.com>
2014-12-07 08:17:11 +0900

    メニューのインデックスをロードする処理を実装。

--- lib/Diary.pm
+++ lib/Diary.pm
@@ -1,10 +1,32 @@
 package Diary;
 use strict;
 use warnings;
+use Time::Piece;
 use Diary::Entry;
+use Diary::Date qw/index_year/;
+
+sub dir_entries {
+    my ($dir, $reg) = @_;
+    my @entries = ();
+    if (opendir my $dh, $dir) {
+        @entries = grep { ! /^\.{1,2}$/ && /$reg/ } readdir($dh);
+        closedir($dh);
+    }
+    return @entries;
+}
+
+my $last_checked_year = 0;
+my $last_years;
 
 sub years {
-    [qw/2014 2013 2012 2011/];
+    my $t = localtime;
+    my $year = index_year($t->year, $t->mon, $t->mday);
+    if (!defined($last_years) || $year != $last_checked_year) {
+        my @dirs = sort { -($a cmp $b) } dir_entries("data", qr/^\d{4}$/);
+        $last_years = \@dirs;
+        $last_checked_year = $year;
+    }
+    return $last_years;
 }
 
 sub entries {
