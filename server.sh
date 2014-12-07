f2b56533eaad82066e6e5055277d07650f4f202c
ktmizugaki <ktmizugaki@gmail.com>
2014-12-07 18:07:23 +0900

    実稼働用のserver設定を追加。

--- server.sh
+++ server.sh
@@ -2,6 +2,8 @@
 
 USER=nginx
 
+mode=$1
+
 mkusrdir() {
     local dir=$1
     if [ -f $dir ]; then
@@ -18,5 +20,23 @@ mkusrdir tmp
 mkusrdir log
 mkusrdir data
 
+if [ x$mode = xstop ]; then
+    if [ -r tmp/diary.pid ]; then
+        kill -TERM $(cat tmp/diary.pid)
+    fi
+    exit $?
+fi
+
+options=
+if [ x$mode = xdaemon ]; then
+    if [ -f tmp/diary.pid ]; then
+        echo try to stop running proccess >&2
+        kill -TERM $(cat tmp/diary.pid)
+    fi
+    options="--pid tmp/diary.pid -D -E deployment"
+else
+    options="--access-log log/access.log -R ./lib,./lib/Diary,./templates,./templates/layouts"
+fi
+
 exec su $USER -s /bin/bash -c 'eval $(perl -Mlocal::lib=$PWD/perl5);
-exec plackup -s FCGI --listen tmp/diary.socket --access-log log/access.log -R ./lib,./lib/Diary,./templates,./templates/layouts ./diary'
+exec plackup -s FCGI -l tmp/diary.socket '"$options"' ./diary'
