31048be648768cc9c3256fc8d66f82f513bb38e0
ktmizugaki <ktmizugaki@gmail.com>
2017-03-04 20:01:59 +0900

    Fix install script
    Make install work with newer perl.
    Install FCGI.

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+export PERL_USE_UNSAFE_INC=1
+
 install() {
     cpanm --local-lib=$PWD/perl5 --notest $1
 }
 
 install Plack
+install FCGI
 install FCGI::ProcManager
 install Linux::Inotify2
 install Mojolicious
